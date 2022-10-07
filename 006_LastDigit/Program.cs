aa6f61300285c376468f0ec76a15ea875d5c89e0
Alexei <datlife2015@yandex.ru>
2022-10-07 15:48:41 +0300

    Показать последнюю цифру трёхзначного числа

---
+++ 006_LastDigit/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.WriteLine("Введите любое трехзначное число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int result = number % 10;
+
+Console.WriteLine("Последней цифрой введеного числа является: ");
+Console.Write(result);
