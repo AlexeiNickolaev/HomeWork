05c8f5a6043dc05de7414f6ce453df7edc8b2720
Alexei <datlife2015@yandex.ru>
2022-10-07 15:26:39 +0300

    Найти максимальное из трех чисел

---
+++ 003_Max_3_num/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти максимальное из трех чисел
+int a = 55;
+int b = 8;
+int c = 12;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
