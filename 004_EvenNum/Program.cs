c15d99cace12ee9bbe4ab350037ee8cc9b38e95f
Alexei <datlife2015@yandex.ru>
2022-10-07 15:33:58 +0300

    Выяснить является ли число чётным

---
+++ 004_EvenNum/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число является четным!");
+}
+else
+{
+    Console.WriteLine("Число не является четным!");
+}
