8070dfeb34b7eb702ce9797b57d0fb28f1619853
Alexei <datlife2015@yandex.ru>
2022-10-07 16:01:00 +0300

    Показать вторую цифру трёхзначного числа

---
+++ 007_SecondDigit/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine() ?? "0" );
+
+int result = (number / 10 % 10);
+
+Console.Write("Вторая цифра введенного вами числа: ");
+Console.Write(result);
