801bd0745f907f5e969caa3ba7ac539f4cf3e1bc
Alexei <datlife2015@yandex.ru>
2022-10-07 16:17:19 +0300

    Удалить вторую цифру трёхзначного числа

---
+++ 009_DelSecDigit/Program.cs
@@ -0,0 +1,7 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int number2 = (number / 100 * 10 + number % 10);
+
+Console.WriteLine("Если удалить вторую цифру из введеного вами числа, то получится: ");
+Console.WriteLine(number2);
